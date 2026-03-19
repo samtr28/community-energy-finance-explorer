6014e0a88eed66be91c05a5a30c62d5216bcdc2c
sam.treacy.28@gmail.com <sam.treacy.28@gmail.com>
2026-03-19 18:04:34 +0000

    Created methods_context.lottie_methods; Edited methods_context, theme.css and settings

--- client_code/methods_context/lottie_methods/__init__.py
+++ client_code/methods_context/lottie_methods/__init__.py
@@ -1,19 +1,15 @@
-from ._anvil_designer import methods_contextTemplate
+from ._anvil_designer import lottie_methodsTemplate
 from anvil import *
-import m3.components as m3
 import anvil.server
+import m3.components as m3
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class methods_context(methods_contextTemplate):
+class lottie_methods(lottie_methodsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
-    def form_show(self, **event_args):
-      """This method is called when the form is shown on the page"""
-    self.layout.reset_links()
-    self.layout.methods_nav.role = 'selected'
+
     # Any code you write here will run before the form opens.
