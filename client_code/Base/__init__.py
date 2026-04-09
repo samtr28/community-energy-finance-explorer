e24610ad4fa1a9a4420587f35bb4885759961752
sam.treacy.28@gmail.com <sam.treacy.28@gmail.com>
2026-04-09 18:11:44 +0000

    Edited theme.css, Base and methods

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -5,9 +5,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
-# Add these imports to your form (for iframe integration)
-from anvil.js.window import jQuery
+from anvil.js.window import jQuery, innerWidth
 from anvil.js import get_dom_node
 
 
@@ -15,6 +13,11 @@ class Base(BaseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    if innerWidth < 998:
+      self.iih_logo.width = 150
+    else:
+      self.iih_logo.width = 300
     # Any code you write here will run before the form opens.
 
   def overview_nav_click(self, **event_args):
