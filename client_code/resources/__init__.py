404205f4063c017cd2957e64ecdaa2026a966f7e
sam.treacy.28@gmail.com <sam.treacy.28@gmail.com>
2026-04-20 18:38:30 +0000

    Edited Base, Resources, projects_explorer.Project_Card, resources and 1 more; Deleted resources.ItemTemplate1

--- client_code/resources/__init__.py
+++ client_code/resources/__init__.py
@@ -9,14 +9,8 @@ from anvil.tables import app_tables
 class resources(resourcesTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
-    self.load_files()
 
   def form_show(self, **event_args):
     """This method is called when the form is shown on the page"""
     self.layout.reset_links()
     self.layout.resources_nav.role = 'selected'
-
-  def load_files(self):
-    """Fetch files from server and populate repeating panel"""
-    files = anvil.server.call('get_pdf_files')
-    self.repeating_panel_1.items = files
