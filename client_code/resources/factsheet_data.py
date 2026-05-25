1210578be7c77a43bc1fb0e8c113abd30a96ec44
sam.treacy.28@gmail.com <sam.treacy.28@gmail.com>
2026-05-25 18:01:47 +0000

    Created Factsheets/.gitkeep, resources.ItemTemplate1, resources.factsheet_data and Factsheets/Community_Bonds_Infographic.png; Edited resources

---
+++ client_code/resources/factsheet_data.py
@@ -0,0 +1,19 @@
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+#
+#    from ..resources import Module1
+#
+#    Module1.say_hello()
+#
+
+factsheets = [
+  {
+    "title": "Community Bonds",
+    "summary": "Community bonds are a debt financing instrument that helps organizations raise capital from community members and other investors to fund revenue generating projects.",
+    "image": "_/theme/Factsheets/Community_Bonds_Infographic.png",  # path to your uploaded image
+    "pdf_url": "https://impactinvestinghub.ca/wp-content/uploads/2026/05/FS-Community-Bonds-May-2026.pdf"
+  }]
